64a43e5e5d94db49bf9b5d74adb264922ba3af11
Nikhil Kushwah <122998371+Nikhilkushw@users.noreply.github.com>
2024-02-05 07:40:40 +0000

    Datatypes

---
+++ 01_basics/Datatype.js
@@ -0,0 +1,15 @@
+"use strict" // treat all js as newer version
+console.log(3
+    +3
+    )
+console.log("Nikhil")
+
+// number => 2 to the power 64
+// bigint
+// string => ""
+// boolean => True/False
+// null => standalone value
+// undefined => 
+// Symbol => unique
+
+console.table([typeof "nikhil",typeof 8786, typeof true,typeof null,typeof undefined])
