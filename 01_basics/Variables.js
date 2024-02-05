be7e6a556557beb39c3696fad7328c0a9b957d62
Nikhil Kushwah <122998371+Nikhilkushw@users.noreply.github.com>
2024-02-05 06:33:57 +0000

    config code done

---
+++ 01_basics/Variables.js
@@ -0,0 +1,13 @@
+const accountId = 3423656
+let accountEmail = "nikhilKush037@gmail.com"
+var accountPassword = "635465"
+accountCity = "Gwalior"
+let accountState;
+let accountCountry = null;
+// Prefer not use var
+// Because of issue in block dcope and functional scope
+
+accountEmail = "nikhilKush@gmail.com"
+accountPassword = "12365"
+accountCity = "Dabra"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState,accountCountry])
